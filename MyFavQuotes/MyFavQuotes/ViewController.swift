1558204a18da01431a60d60d550568d703ab1d10
ClaireRimel <sivadiercl@gmail.com>
2020-05-14 18:23:14 +0200

    New: Create project MyFavQuotes

---
+++ MyFavQuotes/MyFavQuotes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyFavQuotes
+//
+//  Created by Claire on 14/05/2020.
+//  Copyright © 2020 Claire Sivadier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
