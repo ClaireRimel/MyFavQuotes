468082f8be7658ff4adaccf646491abdc899534c
ClaireRimel <sivadiercl@gmail.com>
2020-05-14 20:38:59 +0200

    New: FavQuotes Storyboard and FavQuotesTableViewCell handle elements of the screen

---
+++ MyFavQuotes/MyFavQuotes/FavQuotes/View/FavQuotesTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  TableViewCell.swift
+//  MyFavQuotes
+//
+//  Created by Claire on 14/05/2020.
+//  Copyright © 2020 Claire Sivadier. All rights reserved.
+//
+
+import UIKit
+
+class FavQuotesTableViewCell: UITableViewCell {
+
+    @IBOutlet var bodyLabel: UILabel!
+    @IBOutlet var authorLabel: UILabel!
+    @IBOutlet var idLabel: UILabel!
+    
+}
+
